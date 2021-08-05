4d5d848d931a420bbb5c71ada3a0755bbc55f2ed
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-08-05 14:02:30 +0530

    Add files via upload

---
+++ C9.py
@@ -0,0 +1,38 @@
+# Teacher Activity 1 and Additional Activity 1
+for i in range(5): 
+    print(i)
+    
+for i in range(1,6):
+    print(i)
+        
+        
+# Teacher Activity 2        
+a = ["b","f","b","f","f"]
+for i in a:
+    if i == "b":
+        print(i)
+    elif i == "f":
+        print(i)
+        
+        
+# Student Activity        
+a = ["b","f","b","f","f", "c", "c"]
+for i in a:
+    if i == "b":
+        print(i)
+    elif i == "f":
+        print(i)
+    elif i == "c":
+        print(i)
+        
+
+
+# Additional Activity 2
+import random        
+ring = random.randint(1,18)
+print(ring)
+for i in range(1,19):
+    if i == ring:
+        print(i)
+        break
+    print(i)
